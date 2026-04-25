6c21753a7abe605f5a3b426976b22335179823a8
erzan12 <do.earljan@gmail.com>
2026-04-25 11:03:08 +0800

    feat(views-components): enhanced components and views

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,9 +4,28 @@
 
 {{-- You can call component by adding x from start followed by dash then name of component --}}
 <x-hero/>
-<x-service/>
-<x-specialist/>
-<x-about/>
-<x-feedback/>
+<section class="section-light">
+    <x-service/>
+</section>
 
+<section class="section-alt">
+    <x-specialist/>
+</section>
+
+<section class="cta-section">
+    <div class="container text-center text-light">
+        <h2>Ready to book an appointment?</h2>
+        <a href="{{ route('login') }}" class="btn btn-light mt-3">
+            Get Started
+        </a>
+    </div>
+</section>
+
+<section class="section-light">
+    <x-feedback/>
+</section>
+
+<section class="section-alt">
+    <x-about/>
+</section>
 @endsection
