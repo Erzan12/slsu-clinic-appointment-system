699a2e8e82f3c12c07811411cdd3fc0914b735fa
erzan <do.earljan@gmail.com>
2026-04-25 18:08:51 +0800

    feat(sidebar): user sidebar improve added toggle and icons

--- resources/views/utils/sidebar.blade.php
+++ resources/views/utils/sidebar.blade.php
@@ -7,7 +7,13 @@
     </div>
 
     <nav class="sidebar-nav-wrapper">
-        <a href="{{route('dashboard')}}" class="side-link @if(request()->routeIs('dashboard')) link-active @endif">Home</a>
+        <button id="sidebarToggle" class="btn btn-modern">☰</button>
+        <a href="{{route('dashboard')}}" class="side-link @if(request()->routeIs('dashboard')) link-active @endif">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+            </svg>
+            <span class="link-text">Home</span>
+        </a>
         {{-- @if(!request()->routeIs('profile.*')) --}}
 
             {{-- Add inside all the link related for patient --}}
@@ -29,7 +35,13 @@
 
             {{-- Add inside all the links related and can be use for patient and specialist --}}
             @if(auth()->user()->account_type == 2 || auth()->user()->account_type == 3)
-                <a href="{{route('appointments.index')}}" class="side-link @if(request()->routeIs('appointments.*')) link-active @endif">My Appointments</a>
+                <a href="{{route('appointments.index')}}" class="side-link @if(request()->routeIs('appointments.*')) link-active @endif">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
+                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                    <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"/>
+                </svg>
+                    My Appointments
+                </a>
             @endif
         {{-- @else --}}
             {{-- <a href="{{route('profile.information')}}" class="side-link @if(request()->routeIs('profile.information')) link-active @endif">My Information</a> --}}
@@ -37,3 +49,11 @@
         {{-- @endif --}}
     </nav>
 </div>
+<script>
+  const toggleBtn = document.getElementById('sidebarToggle');
+  const sidebar = document.querySelector('.sidebar-wrapper');
+
+  toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar-collapsed');
+  });
+</script>
