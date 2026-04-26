67796f718513c52ae3840af65a21a6f060c99133
erzan <do.earljan@gmail.com>
2026-04-26 19:02:52 +0800

    feat(edit-services): added edit action for manage services admin view

--- resources/views/admins/services/edit.blade.php
+++ resources/views/admins/services/edit.blade.php
@@ -3,28 +3,33 @@
 @section('content')
 <div class="container">
     @if (session('success'))
-        <div class="alert alert-success">{{$message}}</div>
     @endif
-    <h1>Edit Service record: {{ $service->name }}</h1>
+    <h1>Update Service details  {{ $service->name }}</h1>
 
-    <form action="{{ route('service.update', $service->id) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('services.update', $service->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
-        <input type="hidden" class="form-control" name="id" value="{{Crypt::encrypt($service->id)}}">
         <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input type="file" class="form-control" id="image" name="image" required>
+
+            @if($service->image)
+                <div class="mb-2">
+                    <img src="{{ asset('storage/'.$service->image) }}" width="120">
+                </div>
+            @endif
+
+            <input type="file" class="form-control" id="image" name="image">
         </div>
         <div class="mb-3">
-            <label for="service" class="form-label">Service</label>
-            <input type="text" class="form-control" id="service" name="service" value="{{ old('service', $service->service) }}" required>
+            <label for="name">Service</label>
+            <input type="text" id="name" name="name" value="{{ old('name', $service->name) }}" required>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <input type="text" class="form-control" id="description" name="description" value="{{ old('description', $service->description) }}">
         </div>
 
-        <button type="submit" class="btn btn-primary">Update Record</button>
+        <button type="submit" class="btn btn-primary">Update Details</button>
     </form>
 </div>
 @endsection
