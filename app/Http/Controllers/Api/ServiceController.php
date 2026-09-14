35629eec1df55a24dcaba6f3178c78c81fe60163
erzan12 <do.earljan@gmail.com>
2026-09-14 10:00:54 +0800

    feat: added new service controller with get services api

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Service;
 
 class ServiceController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Service::where('is_active', true)->get());
+    }
 }
